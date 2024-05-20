2f38808bd5235416f778c9a91310c93e8c2f80da
Soham Maity <sohammaity006@gmail.com>
2024-05-20 19:24:31 +0530

    Updated Files

---
+++ Practice/2.js
@@ -0,0 +1,18 @@
+const prompt = require("prompt-sync")();
+
+// let a = 0;
+// let sum = 0;
+
+// a = prompt("Enter A Number :  ");
+
+// a = parseInt(a);
+
+// for (let i = 1; i <= a; i++) {
+//     sum += i;
+//     }
+
+//     console.log("Sum OF 1 To " + a + " Is : " + sum);
+
+// While Loop
+let a = 0;
+let sum = 0;
