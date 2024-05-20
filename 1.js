6091448ebe677af277f590f361721ccdc4420ff9
Soham Maity <sohammaity006@gmail.com>
2024-05-20 19:20:51 +0530

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,31 @@
+let t = "SOHAM";
+let n = 90;
+
+c = t + n;
+
+console.log(c);
+console.log(typeof c);
+
+const a = {
+    "name": "SOHAM",
+    "age": 17
+}
+
+console.log(a);
+
+// Add New Key
+a["isStudent"] = true;
+
+console.log(a);
+
+// Word Meanig Dictornary
+const wordmeaning = {
+    abandon: "To Leave",
+    "abate": "To Reduce",
+    "abdicate": "To Give Up",
+    "aberration": "Something Unusual",
+    "abhor": "To Hate"
+}
+
+console.log(wordmeaning.abandon);
+console.log(wordmeaning["abandon"]);
